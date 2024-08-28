982dd49b28337ef643c5f24f8cea1725d7a8c8f3
Bharat Dua <bharatdua13@gmail.com>
2024-08-28 10:58:21 +0530

    added the start page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,48 @@
+import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from "./StartScreen";
 
+const initialState = {
+  questions: [],
+  status: "loading",
+};
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "loaded" };
+    case "dataError":
+      return { ...state, status: "error" };
+    default:
+      throw new Error("action unknown");
+  }
+}
 function App() {
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+  const numQuestions = questions.length;
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const respone = await fetch("http://localhost:9000/questions");
+        const data = await respone.json();
+        dispatch({ type: "dataReceived", payload: data });
+      } catch (error) {
+        console.error("Error while fetching", error);
+        dispatch({ type: "dataError" });
+      }
+    }
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
       <Header />
       <Main>
-        <p>1/3</p>
-        <p>questions</p>
+        {status === "loading" && <Loader />}
+        {status === "loaded" && <StartScreen numQuestions={numQuestions} />}
+        {status === "error" && <Error />}
       </Main>
     </div>
   );
---
+++ src/StartScreen.js
@@ -0,0 +1,11 @@
+function StartScreen({ numQuestions }) {
+  return (
+    <div className="start">
+      <h2>Welcome to the react quiz!</h2>
+      <h3>{numQuestions} questions to test your react mastery!</h3>
+      <button className="btn btn-ui">let's start</button>
+    </div>
+  );
+}
+
+export default StartScreen;
